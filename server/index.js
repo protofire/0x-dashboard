2e2b265f2635d04bf76190adb85e5c368a9b7d58
Senkevich Alexsandr <a.siankevich@altoros.com>
2018-08-01 15:28:37 +0300

    refactor getting data

--- server/index.js
+++ server/index.js
@@ -45,19 +45,19 @@ let web3 = new Web3(ZeroClientProvider({getAccounts: (cb) => { cb(null, []); }, 
 
 app.use(cors()); // enable CORS
 
-// app.use('/api', [authController]);
-app.use('/', express.static(path.join(__dirname, '../build')));
 
-app.get('/', function(req,res){
-    res.sendFile(path.join(__dirname, '../build/index.html'));
-});
-
-app.get('/trades', function(req,res){
+app.get('/api/trades', function(req,res){
 
     res.json({
         success: true,
         trades: tradeService.getLast24TradeData()
     })
+});
+
+app.use('/', express.static(path.join(__dirname, '../build')));
+
+app.get('/*', function(req,res){
+    res.sendFile(path.join(__dirname, '../build/index.html'));
 });
 
 
